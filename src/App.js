760f6fc3052161274cf9a8122414543867da9ab7
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-02-21 20:29:49 +0000

    inside button add the onSubmit function which will fetch the data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
 
 function App() {
+
+  const handleSubmit = () {
+    fetch()
+  }
+
+
+
   return (
     <form action="">
       <label>Register to APP</label>
+      <button onSubmit={handleSubmit}>Click to Register</button>
     </form>
   );
 }
