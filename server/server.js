2692bf7150112394b354ebdd8840d0a8f1903344
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-02-23 19:05:17 +0000

    added

--- server/server.js
+++ server/server.js
@@ -1,3 +1,9 @@
 const express = require("express");
 
 const app = express();
+const port = 3001;
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const handleSubmit = () => {};
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ email, password });
+  };
+
   return (
-    <form action="">
-      <label>Register to APP</label>
-      <button onSubmit={handleSubmit}>Click to Register</button>
+    <form onSubmit={handleSubmit}>
+      <label>Enter email:</label>
+      <input
+        type="text"
+        required
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Type your email"
+      />
+      <label>Enter password:</label>
+      <input
+        type="text"
+        required
+        value={password}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Type your password"
+      />
+
+      <button type="submit">Click to Register</button>
     </form>
   );
 }
