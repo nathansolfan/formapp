88297e84a36535dacfadcc18c95e448db9d14d7a
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-02-23 19:21:27 +0000

    Correcly communicate with the server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 const port = 3001;
+app.use(cors());
 app.use(express.json());
+
+// Define the POST route for /register
+app.post("/register", (req, res) => {
+  const { email, password } = req.body;
+
+  console.log(`Registering user with ${email}, and ${password}`);
+
+  res.json({ message: "Registration successful" });
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
