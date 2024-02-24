e66bd20c6430506a8ebc10e989c4f29356d7c7ea
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-02-24 19:33:43 +0000

    create the local json to act as database. Then require path and fs(FileSystem) where we join the json into a userFilePath const and then use the fs.readFIle method on it, where error and data are the 2 params

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const cors = require("cors");
+// FileSystem and path
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
 const port = 3001;
+
 app.use(cors());
 app.use(express.json());
 
 // Define the POST route for /register
 app.post("/register", (req, res) => {
   const { email, password } = req.body;
+
+  // path to the users.json
+  const userFilePath = path.join(__dirname, "users.json");
+  // read
+  fs.readFile(userFilePath, (err, data) => {
+    if (err) {
+      res.status(500).json({ message: "Error reading the dAta" });
+      return;
+    }
+  });
 
   console.log(`Registering user with ${email}, and ${password}`);
 
