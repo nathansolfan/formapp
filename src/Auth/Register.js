96bba3693ff2bdad4581c66d031f94e471efaf7a
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-02-25 19:03:31 +0000

    Create the whole fetch login on the Registration form, where email and password is taken from the Form component

--- src/Auth/Register.js
+++ src/Auth/Register.js
@@ -1,26 +1,24 @@
 import React from "react";
+import Form from "../Form";
 
 export default function Register() {
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const response = await fetch("http://localhost:3001/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response not ok");
-        }
-        return response.json();
-      })
-      .then((data) => console.log(data + " daTa 1"))
-      .catch((error) => console.error("There has been a probrem"));
-
-    console.log({ email, password });
+  const handleRegistration = async ({ email, password }) => {
+    try {
+      const response = await fetch("http://localhost:3001/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      if (!response) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json;
+      console.log(data, "Registration ok");
+    } catch (error) {
+      console.error("There is a problem here");
+    }
   };
-
-  return <div>Register</div>;
+  return <Form />;
 }
