a556ec8aa9c4cd46d723ba9d4344dc5a76b5040d
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-02-27 20:11:15 +0000

    Created the login component where Form is imported and from it the {} email and password

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Login from "./Auth/Login";
 import Register from "./Auth/Register";
 
 function App() {
   return (
     <div>
       <Register />
+      <Login />
     </div>
   );
 }
--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Form from "../Form";
 
-export default function Login() {
-  return <div>Login</div>;
+export default function Login({ email, password }) {
+  return (
+    <div>
+      <Form buttonText="Button Login" />
+    </div>
+  );
 }
