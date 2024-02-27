cd9bb8b99c371c6ba091687e08e074d4bf62397c
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-02-27 20:13:48 +0000

    create the handleLogin for the onSubmit where it fetchs the POST data

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Form from "../Form";
 
-export default function Login({ email, password }) {
+export default function Login() {
+  const handleLogin = async ({ email, password }) => {
+    try {
+      const response = await fetch("http://localhost:3001/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+    } catch (error) {}
+  };
   return (
     <div>
-      <Form buttonText="Button Login" />
+      <Form onSubmit={handleLogin} buttonText="Button Login" />
     </div>
   );
 }
