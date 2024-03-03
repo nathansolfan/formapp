7beaf9584e435b7a9be3cc3eddca16710bd87f23
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-03-03 19:32:29 +0000

    Logout correctly removes the userEmail created

--- src/Auth/Logout.js
+++ src/Auth/Logout.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function Logout() {
   const logout = () => {
     localStorage.removeItem("accessToken");
+    localStorage.removeItem("userEmail");
   };
   return (
     <div>
